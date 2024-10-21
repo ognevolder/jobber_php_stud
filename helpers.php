0ac3fc16ddee0ed0556129e58a59852e0e4dc4fa
ognevolder <ognevolder.webdev@gmail.com>
2024-10-21 17:52:02 +0200

    Initialization. Home-view added

---
+++ helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Get the base path of the file
+ * 
+ * In
+ * @param string $path
+ * Out
+ * @return string
+ */
+
+function basePath($path = '')
+{
+  return __DIR__ . '/' . $path;
+}
